34ac00f685dd2f7bc6695ace1c6bafe5b83508ba
SherwinKhoo <99573291+SherwinKhoo@users.noreply.github.com>
2022-04-08 09:12:35 +0800

    Added separate folders for frontend and backend.
    index.html file has bootstrap, jQuery and font dependencies.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Suspense } from "react";
+import { Route, Navigate, Routes, useNavigate } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Project 3</h2>
+    </div>
+  );
+};
+
+export default App;
