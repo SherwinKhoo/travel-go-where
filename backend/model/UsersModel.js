fc5ad2ef323e92ebd709128590efaaa8e1360c6b
sgamirul <a.mirulhityou@hotmail.com>
2022-04-08 10:10:52 +0800

    user schema
    - name
    - password
    - validate name
    - admin
    - profilePic

---
+++ backend/model/UsersModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const usersSchema = Schema({
+  name: {
+    type: String,
+    required: [true, "Empty field not allowed"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Empty field not allowed"],
+    min: [6, "password too short"],
+  },
+  validate: {
+    validator: /^[a-zA-Z0-9_]{3,16}$/,
+    message:
+      "Usernames must be 3 to 16 characters long and contain only alphanumeric characters and underscores (_).",
+  },
+  admin: { type: false, required: true },
+  profilePic: { type: String },
+});
+
+const Users = mongoose.model("Users", usersSchema);
+
+module.exports = Users;
