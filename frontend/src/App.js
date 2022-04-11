d120cbe91e4b3d1ff8b86cc64ce1e9dbb73992b6
yingyingquek <quekyingying26@gmail.com>
2022-04-11 10:43:30 +0800

    main threads commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React, { Suspense } from "react";
 import { Route, Navigate, Routes, useNavigate } from "react-router-dom";
 import LandingPage from "./components/landing_page/LandingPage";
+import MainThread from "./components/country_threads/MainThread";
 
 const App = () => {
   return (
-    <>
-      <LandingPage />
-    </>
+    <main>
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/Singapore" element={<MainThread />}></Route>
+      </Routes>
+    </main>
   );
 };
 
---
+++ frontend/src/components/country_threads/MainThread.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const MainThread = (props) => {
+  const [topic, setTopic] = useState("");
+
+  const enterTopics = async (topic) => {
+    const requestOptions = {
+      method: "GET",
+    };
+
+    const url = `http://127.0.0.1:5001/${props.country}/${topic}`;
+    const response = await fetch(url, requestOptions);
+    const data = response.json();
+    console.log(data);
+  };
+
+  const handleSubmitToTopics = (event) => {
+    setTopic(event.target.value);
+    enterTopics(event.target.value);
+  };
+
+  return (
+    <>
+      <input type="text" placeholder="Search for a topic"></input>
+      <div>
+        <h2>Welcome Center</h2>
+        <li onClick={handleSubmitToTopics}>Rules</li>
+      </div>
+      <div>
+        <h2>Popular Places to Go</h2>
+      </div>
+      <div>
+        <h2>General Discussions</h2>
+      </div>
+    </>
+  );
+};
+
+export default MainThread;
