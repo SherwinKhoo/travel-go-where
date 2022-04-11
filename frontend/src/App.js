9ac7b820e2f1a7242ce121060e73920bd1cee0e2
yingyingquek <quekyingying26@gmail.com>
2022-04-11 11:20:53 +0800

    updates to router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,27 @@ import React, { useState, Suspense } from "react";
 import { Route, Navigate, Routes, useNavigate } from "react-router-dom";
 import LandingPage from "./components/landing_page/LandingPage";
 import MainThread from "./components/country_threads/MainThread";
+import LoginPage from "./components/login/LoginPage";
 
 const App = () => {
   const [searchCountry, setSearchCountry] = useState("");
 
   return (
-    <main>
-      <Routes>
-        <Route
-          path="/"
-          element={<LandingPage setSearchCountry={setSearchCountry} />}
-        ></Route>
-        <Route
-          path={`/${searchCountry}`}
-          element={<MainThread searchCountry={searchCountry} />}
-        ></Route>
-      </Routes>
-    </main>
+    <>
+      <LoginPage />
+      <main>
+        <Routes>
+          <Route
+            path="/"
+            element={<LandingPage setSearchCountry={setSearchCountry} />}
+          ></Route>
+          <Route
+            path={`/${searchCountry}`}
+            element={<MainThread searchCountry={searchCountry} />}
+          ></Route>
+        </Routes>
+      </main>
+    </>
   );
 };
 
