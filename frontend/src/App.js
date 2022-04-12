df9500cc102bebad32ba6376ffb6c497a17a9fe7
yingyingquek <quekyingying26@gmail.com>
2022-04-12 11:05:18 +0800

    update useContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,50 @@
-import React, { useState, Suspense } from "react";
+import React, { useState, useContext, Suspense } from "react";
 import { Route, Navigate, Routes, useNavigate } from "react-router-dom";
 import LandingPage from "./components/landing_page/LandingPage";
 import MainThread from "./components/country_threads/MainThread";
 import LoginPage from "./components/login/LoginPage";
 import IndividualPost from "./components/country_threads/IndividualPost";
+import PopularPlaces from "./components/country_threads/PopularPlaces";
+import resultsContext from "./components/context/resultsContext";
 
 const App = () => {
   const [searchCountry, setSearchCountry] = useState("");
-  // const [topic, setTopic] = useState("");
+  const [topic, setTopic] = useState("");
+  const [results, setResults] = useState([]);
 
   return (
     <>
       <LoginPage />
-      <main>
-        <Routes>
-          <Route
-            path="/"
-            element={<LandingPage setSearchCountry={setSearchCountry} />}
-          ></Route>
-          <Route
-            path={`/${searchCountry}`}
-            element={<MainThread searchCountry={searchCountry} />}
-          ></Route>
-          {/* <Route
-            path={`/${searchCountry}/${topic}`}
-            element={<IndividualPost topic={topic} />}
-          ></Route> */}
-        </Routes>
-      </main>
+      <resultsContext.Provider value={{ results, setResults }}>
+        <main>
+          <Routes>
+            <Route
+              path="/"
+              element={<LandingPage setSearchCountry={setSearchCountry} />}
+            ></Route>
+            <Route
+              path={`/${searchCountry}`}
+              element={
+                <MainThread
+                  searchCountry={searchCountry}
+                  topic={topic}
+                  setTopic={setTopic}
+                />
+              }
+            ></Route>
+            <Route
+              path={"/Singapore/0/:topic/:index"}
+              element={
+                <IndividualPost topic={topic} value={{ results, setResults }} />
+              }
+            ></Route>
+            <Route
+              path={`/${searchCountry}/1`}
+              element={<PopularPlaces />}
+            ></Route>
+          </Routes>
+        </main>
+      </resultsContext.Provider>
     </>
   );
 };
