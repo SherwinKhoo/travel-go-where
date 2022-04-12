9ff8a1a7e5b7977d8ecfcc4f7dacb75a08625a34
sherwinkhoo <khoo.sherwin@gmail.com>
2022-04-12 13:17:33 +0800

    Updated IndividualPost1/2/3.js. Updated with delete buttons. Need to look into the fetch path.

--- frontend/src/components/country_threads/IndividualPost2.js
+++ frontend/src/components/country_threads/IndividualPost2.js
@@ -5,6 +5,7 @@ const IndividualPost2 = (props) => {
   const params = useParams();
   console.log(props.value.results2[params.index]);
   const displayPost = props.value.results2[params.index];
+  console.log(displayPost);
 
   const tags = displayPost.categories.map((list, index) => {
     return (
@@ -13,13 +14,43 @@ const IndividualPost2 = (props) => {
       </>
     );
   });
+
+  const deletePost = async (id) => {
+    const res = await fetch(
+      `http:127.0.0.1:5001/topics/Singapore/0/${id}/delete`,
+      {
+        method: "delete",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (res.status === 200) {
+      console.log("post deleted");
+      // code to re-render the page??
+    }
+  };
+
+  const handleDelete = (event) => {
+    deletePost(displayPost._id);
+    console.log(displayPost._id);
+  };
+
   return (
     <>
       <div className="container individualPostContainer">
-        <h5 className="individualPostId">#{displayPost.postId}</h5>
+        <h5 className="col-md-1 individualPostId">#{displayPost.postId}</h5>
         <h1>{displayPost.title}</h1>
         <h5>{displayPost.content}</h5>
         <ul>{tags}</ul>
+        <button className="col-md-1 btn individualPostEdit">Edit</button>
+        <button
+          className="col-md-1 btn individualPostDelete"
+          // value={displayPost._id}
+          onClick={handleDelete}
+        >
+          Delete
+        </button>
       </div>
     </>
   );
