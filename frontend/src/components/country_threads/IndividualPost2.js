8db6a0774dff17dbaa2c1ac10bc968e736d4aaa1
sherwinkhoo <khoo.sherwin@gmail.com>
2022-04-12 16:12:08 +0800

    Updated IndividualPost1/2/3.js to allow edit posts.

--- frontend/src/components/country_threads/IndividualPost2.js
+++ frontend/src/components/country_threads/IndividualPost2.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 const IndividualPost2 = (props) => {
+  const [content, setContent] = useState("");
+  const [edit, setEdit] = useState(false);
+
   const params = useParams();
   console.log(props.value.results2[params.index]);
   const displayPost = props.value.results2[params.index];
   console.log(displayPost);
 
-  let nagivate = useNavigate();
+  let navigate = useNavigate();
 
   const tags = displayPost.categories.map((list, index) => {
     return (
@@ -16,6 +19,24 @@ const IndividualPost2 = (props) => {
       </>
     );
   });
+
+  const editPost = async (id) => {
+    const res = await fetch(
+      `http://127.0.0.1:5001/topics/Singapore/0/${id}/edit`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: content,
+        }),
+      }
+    );
+    if (res.status === 200) {
+      console.log("post edited");
+    }
+  };
 
   const deletePost = async (id) => {
     const res = await fetch(
@@ -32,26 +53,74 @@ const IndividualPost2 = (props) => {
     }
   };
 
-  const handleDelete = (event) => {
+  const handleEditClick = (event) => {
+    event.preventDefault();
+    setEdit(true);
+  };
+
+  const handleDeleteClick = (event) => {
     deletePost(displayPost._id);
-    nagivate("/Singapore");
+    navigate("/Singapore");
+  };
+
+  const handleContentChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmitClick = (event) => {
+    event.preventDefault();
+    editPost(displayPost._id);
+    navigate(`/Singapore`);
   };
 
   return (
     <>
       <div className="container individualPostContainer">
-        <h5 className="col-md-1 individualPostId">#{displayPost.postId}</h5>
-        <h1>{displayPost.title}</h1>
-        <h5>{displayPost.content}</h5>
-        <ul>{tags}</ul>
-        <button className="col-md-1 btn individualPostEdit">Edit</button>
-        <button
-          className="col-md-1 btn individualPostDelete"
-          // value={displayPost._id}
-          onClick={handleDelete}
-        >
-          Delete
-        </button>
+        <div className="individualPostHead">
+          <h1>{displayPost.title}</h1>
+        </div>
+        {edit ? (
+          <div className="individualPostForm">
+            <h5 className="col-md-1 individualPostId">#{displayPost.postId}</h5>
+            <input
+              onChange={handleContentChange}
+              defaultValue={displayPost.content}
+            ></input>
+            <ul>
+              <li className="individualPostTags">edited</li>
+              {tags}
+            </ul>
+            <div className="col-md-1"></div>
+            <button
+              type="submit"
+              className="col-md-1 btn individualPostSubmit"
+              value={displayPost._id}
+              onClick={handleSubmitClick}
+            >
+              Submit
+            </button>
+          </div>
+        ) : (
+          <div className="individualPostPost">
+            <h5 className="col-md-1 individualPostId">#{displayPost.postId}</h5>
+            <h5>{displayPost.content}</h5>
+            <ul>{tags}</ul>
+            <button
+              onClick={handleEditClick}
+              onChange={handleContentChange}
+              className="col-md-1 btn individualPostEdit"
+            >
+              Edit
+            </button>
+            <button
+              className="col-md-1 btn individualPostDelete"
+              // value={displayPost._id}
+              onClick={handleDeleteClick}
+            >
+              Delete
+            </button>
+          </div>
+        )}
       </div>
     </>
   );
